ee75a122d7e01e7993e8efd3656d6bbd47246096
Pedro Balestra <pedro.balestra@outlook.com>
2023-04-07 18:15:16 -0300

    Como salvar o texto nas variáveis

--- Curso Flutter/curso_flutter_danki_code_intermediario/lib/pages/home.dart
+++ Curso Flutter/curso_flutter_danki_code_intermediario/lib/pages/home.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController txtnome = TextEditingController();
+
+  TextEditingController txtidade = TextEditingController();
+
+  var nome;
+  var idade;
+
+  void exibeTexto() {
+    setState(() {
+      nome = txtnome.text;
+      idade = int.parse(txtidade.text);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +34,7 @@ class Home extends StatelessWidget {
         child: Column(
           children: [
             TextField(
+              controller: txtnome,
               decoration: const InputDecoration(labelText: "Digite seu nome"),
               style: const TextStyle(
                 fontSize: 18,
@@ -23,11 +43,31 @@ class Home extends StatelessWidget {
               maxLength: 10,
               obscureText: false,
               obscuringCharacter: "*",
-              keyboardType: TextInputType.multiline,
-              onSubmitted: (valor) {
-                print("Digitou $valor");
-              },
+              keyboardType: TextInputType.text,
             ),
+            TextField(
+              controller: txtidade,
+              decoration: const InputDecoration(labelText: "Digite sua idade"),
+              style: const TextStyle(
+                fontSize: 18,
+                color: Colors.green,
+              ),
+              maxLength: 10,
+              obscureText: false,
+              obscuringCharacter: "*",
+              keyboardType: TextInputType.number,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                exibeTexto();
+              },
+              child: const Text("Exibir"),
+            ),
+            Text("O seu nome é: $nome"),
+            Text("A sua idade é: $idade"),
           ],
         ),
       ),
