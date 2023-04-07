f6c34eb4e73fa67c88a683ebe15bc9abf670c520
Pedro Balestra <pedro.balestra@outlook.com>
2023-04-07 18:28:41 -0300

    Manipulando checkbox

--- Curso Flutter/curso_flutter_danki_code_intermediario/lib/pages/home.dart
+++ Curso Flutter/curso_flutter_danki_code_intermediario/lib/pages/home.dart
@@ -8,66 +8,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  TextEditingController txtnome = TextEditingController();
-
-  TextEditingController txtidade = TextEditingController();
-
-  var nome;
-  var idade;
-
-  void exibeTexto() {
-    setState(() {
-      nome = txtnome.text;
-      idade = int.parse(txtidade.text);
-    });
-  }
+  bool cksalada = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Exemplo TextField"),
+        title: const Text("Exemplo CheckBox"),
         backgroundColor: Colors.green,
       ),
       body: Container(
         padding: const EdgeInsets.all(80),
         child: Column(
           children: [
-            TextField(
-              controller: txtnome,
-              decoration: const InputDecoration(labelText: "Digite seu nome"),
-              style: const TextStyle(
-                fontSize: 18,
-                color: Colors.green,
-              ),
-              maxLength: 10,
-              obscureText: false,
-              obscuringCharacter: "*",
-              keyboardType: TextInputType.text,
-            ),
-            TextField(
-              controller: txtidade,
-              decoration: const InputDecoration(labelText: "Digite sua idade"),
-              style: const TextStyle(
-                fontSize: 18,
-                color: Colors.green,
-              ),
-              maxLength: 10,
-              obscureText: false,
-              obscuringCharacter: "*",
-              keyboardType: TextInputType.number,
+            const Text("Lache X Salada"),
+            Checkbox(
+              value: cksalada,
+              onChanged: (bool? valor) {
+                setState(() {
+                  cksalada = valor ?? false;
+                });
+              },
+              activeColor: Colors.red,
             ),
             const SizedBox(
               height: 30,
             ),
             ElevatedButton(
               onPressed: () {
-                exibeTexto();
+                if (cksalada) {
+                  print("O check box foi selecionado");
+                } else {
+                  print("Check box desmarcado");
+                }
               },
               child: const Text("Exibir"),
             ),
-            Text("O seu nome é: $nome"),
-            Text("A sua idade é: $idade"),
           ],
         ),
       ),
