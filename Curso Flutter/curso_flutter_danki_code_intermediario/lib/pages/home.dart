95fd595e62f09e99b750360f4f59828b130ea6c5
Pedro Balestra <pedro.balestra@outlook.com>
2023-04-07 19:53:48 -0300

    Manipulando o SwitchListTile

--- Curso Flutter/curso_flutter_danki_code_intermediario/lib/pages/home.dart
+++ Curso Flutter/curso_flutter_danki_code_intermediario/lib/pages/home.dart
@@ -8,25 +8,55 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  bool opescolhida = false;
+  bool op1 = false;
+  bool op2 = false;
+  bool op3 = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Exemplo Switch"),
+        title: const Text("Exemplo SwitchLitTile"),
         backgroundColor: Colors.green,
       ),
-      body: Row(
+      body: Column(
         children: [
-          const Text("Salvar imagens"),
-          Switch(
+          SwitchListTile(
+            title: const Text("Salvar mensagens"),
+            subtitle: const Text("Salva as mensagens no celular"),
+            secondary: const Icon(Icons.favorite, color: Colors.red),
             activeColor: Colors.green,
-            value: opescolhida,
+            value: op1,
             onChanged: (bool? valor) {
               print("Selecionou: " + valor.toString());
               setState(() {
-                opescolhida = valor!;
+                op1 = valor!;
+              });
+            },
+          ),
+          SwitchListTile(
+            title: const Text("Salvar mensagens"),
+            subtitle: const Text("Salva as mensagens no celular"),
+            secondary: const Icon(Icons.favorite, color: Colors.red),
+            activeColor: Colors.green,
+            value: op2,
+            onChanged: (bool? valor) {
+              print("Selecionou: " + valor.toString());
+              setState(() {
+                op2 = valor!;
+              });
+            },
+          ),
+          SwitchListTile(
+            title: const Text("Salvar mensagens"),
+            subtitle: const Text("Salva as mensagens no celular"),
+            secondary: const Icon(Icons.favorite, color: Colors.red),
+            activeColor: Colors.green,
+            value: op3,
+            onChanged: (bool? valor) {
+              print("Selecionou: " + valor.toString());
+              setState(() {
+                op3 = valor!;
               });
             },
           ),
