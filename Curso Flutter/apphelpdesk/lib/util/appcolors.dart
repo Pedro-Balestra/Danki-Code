ef446d0d47e262188fadad31c542eac6020eb495
Pedro Balestra <pedro.balestra@outlook.com>
2023-07-27 19:59:33 -0300

    Criando AppColors da aplicação

---
+++ Curso Flutter/apphelpdesk/lib/util/appcolors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryColor = const Color(0xFF3C74A6);
+  static Color textColorBlue = const Color(0xFF2D80C9);
+  static Color textColorRed = const Color(0xFFA63C3C);
+  static Color buttonRed = const Color(0xFFA63C3C);
+  static Color backgroudCard = const Color(0xFFF0F2F5);
+  static Color textDescripitionCard = const Color(0xFF969CB2);
+  static Color primaryOpacityColor = const Color.fromRGBO(60, 116, 166, 0.09);
+}
