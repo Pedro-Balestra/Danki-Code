d19a17b87bfc9407254bdec601174bfc5de832f9
Pedro Balestra <pedro.balestra@outlook.com>
2023-07-27 19:48:27 -0300

    Criando Projeto/Criando e testando o banco de dados

---
+++ Curso Flutter/apphelpdesk/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/home.dart';
+
+void main() async {
+  //Função para inicializar o firebase (segundo a documentação)
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  //Para testarmos vamos iniserir um usuario
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  db.collection("usuarios").doc("002").set({"nome": "Jão da esquina"});
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aplicativo HelpDesk',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
